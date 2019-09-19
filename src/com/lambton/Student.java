1c19e42b4b4ec393eaded47c3a257521e7a2dbbc
camysandhu <fmsharmn@gmail.com>
2019-09-18 20:55:39 -0400

    Student class with main

--- src/com/lambton/Student.java
+++ src/com/lambton/Student.java
@@ -2,6 +2,39 @@ package com.lambton;
 
 public class Student
 {
-    int studentid;
+    int studentId;
     String studentName;
+
+    void setvalues()
+    {
+       studentId = 1;
+        studentName = "Harmanpreet Singh";
+
+        studentId = 2;
+        studentName = "SimranJit Singh";
+
+        //studentId = 3;
+       // studentName = "Sachin Singh";
+
+
+
+
+    }
+
+    void display()
+
+    {
+        System.out.println("StudentId : " + studentId);
+        System.out.println("StudentName : " + studentName);
+
+    }
+
+    {
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" Harmanpreet Singh");
+        }
+    }
+
 }
